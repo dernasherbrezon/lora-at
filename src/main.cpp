39aba7e8497db613aba917bea862aa2692a57a97
dernasherbrezon <rodionovamp@mail.ru>
2022-05-28 19:01:13 +0100

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
+#include <Arduino.h>
 #include <AtHandler.h>
 #include <LoRaModule.h>
 #include <esp32-hal-log.h>
 #include <time.h>
-#include <Arduino.h>
 
 #include "Display.h"
 
@@ -18,55 +18,6 @@ LoRaModule *lora = NULL;
 AtHandler *handler;
 Display *display = NULL;
 
-bool chipInitializationFailed = false;
-
-const char *getStatus() {
-  // SETUP - waiting for AP to initialize
-  // CONNECTING - connecting to WiFi
-  // RECEIVING - lora is listening for data
-  // IDLE - module is waiting for rx/tx requests
-  // CHIP FAIL - chip initialization failed
-  if (chipInitializationFailed) {
-    return "CHIP FAIL";
-  }
-  if (lora != NULL && lora->isReceivingData()) {
-    return "RECEIVING";
-  } else {
-    return "IDLE";
-  }
-}
-
-void handleStatus() {
-  // if (!web->authenticate(conf->getUsername(), conf->getPassword())) {
-  //   web->requestAuthentication();
-  //   return;
-  // }
-  // StaticJsonDocument<256> json;
-  // json["status"] = getStatus();
-  // int8_t sxTemperature;
-  // if (lora->getTempRaw(&sxTemperature) == 0) {
-  //   json["chipTemperature"] = sxTemperature;
-  // }
-
-  // Chip *chip = conf->getChip();
-  // if (chip != NULL) {
-  //   if (chip->loraSupported) {
-  //     JsonObject obj = json.createNestedObject("lora");
-  //     obj["minFreq"] = chip->minLoraFrequency;
-  //     obj["maxFreq"] = chip->maxLoraFrequency;
-  //   }
-  //   if (chip->fskSupported) {
-  //     JsonObject obj = json.createNestedObject("fsk");
-  //     obj["minFreq"] = chip->minFskFrequency;
-  //     obj["maxFreq"] = chip->maxFskFrequency;
-  //   }
-  // }
-
-  // String output;
-  // serializeJson(json, output);
-  // web->send(200, "application/json; charset=UTF-8", output);
-}
-
 void setup() {
   Serial.begin(115200);
   log_i("starting. firmware version: %s", FIRMWARE_VERSION);
@@ -76,23 +27,16 @@ void setup() {
 
   lora = new LoRaModule();
   lora->setOnRxStartedCallback([] {
-    display->setStatus(getStatus());
+    display->setStatus("RECEIVING");
     display->update();
   });
   lora->setOnRxStoppedCallback([] {
-    display->setStatus(getStatus());
+    display->setStatus("IDLE");
     display->update();
   });
 
-  Serial.readString();
-
   handler = new AtHandler(lora);
-
-  // web->on("/status", HTTP_GET, []() {
-  //   handleStatus();
-  // });
-
-  display->setStatus(getStatus());
+  display->setStatus("IDLE");
   display->update();
   log_i("setup completed");
 }
