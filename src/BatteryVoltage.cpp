45b0c9d3b73d6318eed0c467562cf5be93685015
dernasherbrezon <dernasherbrezon@gmail.com>
2022-12-03 13:14:41 +0000

    use different default resistors + 0xFF as null

--- src/BatteryVoltage.cpp
+++ src/BatteryVoltage.cpp
@@ -15,7 +15,7 @@
 #endif
 
 #ifndef BATTERY_R1
-#define BATTERY_R1 = 3000
+#define BATTERY_R1 = 4700
 #endif
 
 #ifndef BATTERY_R2
@@ -24,12 +24,17 @@
 
 int readVoltage(uint8_t *result) {
   if (PIN_BATTERY_VOLTAGE == 0) {
-    return -1;
+    *result = 0xFF;
+    return 0;
   }
   // TODO should it be configured? Any timeout needed?
   pinMode(PIN_BATTERY_VOLTAGE, INPUT);
   uint16_t rawValue = analogRead(PIN_BATTERY_VOLTAGE);
-  float voltageDividerIn = (float)rawValue * (BATTERY_R1 + BATTERY_R2) / BATTERY_R2;
+  float voltageDividerIn = (float)(rawValue * 3.3 / 4095) * (BATTERY_R1 + BATTERY_R2) / BATTERY_R2;
+  // not connected
+  if( voltageDividerIn < MIN_BATTERY_VOLTAGE ) {
+    *result = 0xFF;
+  }
   *result = (uint8_t)((voltageDividerIn - MIN_BATTERY_VOLTAGE) / (MAX_BATTERY_VOLTAGE - MIN_BATTERY_VOLTAGE) * 100);
   return 0;
 }
