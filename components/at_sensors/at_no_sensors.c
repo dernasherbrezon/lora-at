fda91f33c8e716e768903d28bcc7fabae20276a1
dernasherbrezon <dernasherbrezon@gmail.com>
2023-12-31 18:43:31 +0000

    make sensors optional

---
+++ components/at_sensors/at_no_sensors.c
@@ -0,0 +1,20 @@
+#include "at_sensors.h"
+
+esp_err_t at_sensors_init() {
+  //do nothing
+  return ESP_OK;
+}
+
+esp_err_t at_sensors_get_solar(uint16_t *bus_voltage, int16_t *current) {
+  //do nothing
+  return ESP_OK;
+}
+
+esp_err_t at_sensors_get_battery(uint16_t *bus_voltage, int16_t *current) {
+  //do nothing
+  return ESP_OK;
+}
+
+void at_sensors_destroy() {
+  //do nothing
+}
