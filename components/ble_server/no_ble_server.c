812b791afffeb56c5d496adf45c579946e5143ad
dernasherbrezon <dernasherbrezon@gmail.com>
2024-05-04 15:49:37 +0100

    fix no ble config

--- components/ble_server/no_ble_server.c
+++ components/ble_server/no_ble_server.c
@@ -1,14 +1,14 @@
 #include "ble_server.h"
 
-esp_err_t ble_server_create(at_sensors *sensors, ble_server **server) {
-  *server = NULL;
+esp_err_t ble_server_create(at_sensors *sensors, sx127x *device, lora_at_config_t *config) {
+  //do nothing
   return ESP_OK;
 }
 
-void ble_server_send_notifications(ble_server *server) {
+void ble_server_send_updates() {
   //do nothing
 }
 
-void ble_server_destroy(ble_server *server) {
+void ble_server_send_frame(sx127x_frame_t *frame) {
   //do nothing
 }
