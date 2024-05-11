9066de380ab78874c38a17a1213282428177543a
dernasherbrezon <dernasherbrezon@gmail.com>
2024-05-11 09:25:55 +0100

    fix no-sensors mode

--- components/at_sensors/at_no_sensors.c
+++ components/at_sensors/at_no_sensors.c
@@ -1,22 +1,40 @@
 #include "at_sensors.h"
 
-esp_err_t at_sensors_init() {
-  //do nothing
+esp_err_t at_sensors_init(at_sensors **dev) {
+  *dev = NULL;
   return ESP_OK;
 }
 
-esp_err_t at_sensors_get_solar(uint16_t *bus_voltage, int16_t *current) {
+esp_err_t at_sensors_get_solar_voltage(uint16_t *bus_voltage, at_sensors *dev) {
   *bus_voltage = 0;
+  return ESP_OK;
+}
+
+esp_err_t at_sensors_get_solar_current(int16_t *current, at_sensors *dev) {
   *current = 0;
   return ESP_OK;
 }
 
-esp_err_t at_sensors_get_battery(uint16_t *bus_voltage, int16_t *current) {
-  *bus_voltage = 0;
-  *current = 0;
+esp_err_t at_sensors_get_solar_power(uint16_t *power, at_sensors *dev) {
+  *power = 0;
   return ESP_OK;
 }
 
-void at_sensors_destroy() {
-  //do nothing
+esp_err_t at_sensors_get_battery_voltage(uint16_t *bus_voltage, at_sensors *dev) {
+  *bus_voltage = 0;
+  return ESP_OK;
+}
+
+esp_err_t at_sensors_get_battery_current(int16_t *current, at_sensors *dev) {
+  *current =0;
+  return ESP_OK;
+}
+
+esp_err_t at_sensors_get_battery_level(uint8_t *level, at_sensors *dev) {
+  *level = 0;
+  return ESP_OK;
+}
+
+void at_sensors_destroy(at_sensors *dev) {
+  //do nmothing
 }
